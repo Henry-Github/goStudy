3b034550e593450090450d5e33e4361929431a5c
henry <liujinlong@yit.com>
2019-08-14 17:15:58 +0800

    use struct test

--- charpters/chapter_26.go
+++ charpters/chapter_26.go
@@ -7,15 +7,19 @@ import (
 
 // channel select
 
-func receiveMultiChan(c1 <-chan string, c2 <-chan string) {
+type Id struct {
+	id   int
+	name string
+}
+
+func receiveMultiChan(c1 <-chan Id, c2 <-chan Id) {
 	for {
 		time.Sleep(time.Second)
 		select {
 		case rec := <-c1:
-			fmt.Printf("receiving from c1 content is : %s\n", rec)
+			fmt.Printf("receiving from c1 content is : %s-%d\n", rec.name, rec.id)
 		case rec := <-c2:
-			fmt.Printf("receiving from c2 content is : %s\n", rec)
-
+			fmt.Printf("receiving from c2 content is : %s-%d\n", rec.name, rec.id)
 		}
 	}
 }
@@ -27,16 +31,16 @@ func waitInput() {
 }
 
 func main() {
-	c1 := make(chan string, 1)
-	c2 := make(chan string, 2)
-	go func(c chan string, name string) {
+	c1 := make(chan Id, 1)
+	c2 := make(chan Id, 1)
+	go func(c chan Id, name string) {
 		for i := 0; ; i++ {
-			c <- name
+			c <- Id{id: i, name: name}
 		}
 	}(c1, "c1")
-	go func(c chan string, name string) {
+	go func(c chan Id, name string) {
 		for i := 0; ; i++ {
-			c <- name
+			c <- Id{id: i, name: name}
 		}
 	}(c2, "c2")
 	go receiveMultiChan(c1, c2)
